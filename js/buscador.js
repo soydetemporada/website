406511dfd7d0a8b72da9cf55867ab88f93929478
Diego Ramírez <lowfill@gmail.com>
2017-09-23 17:31:13 +0200

    Se agregó la funcionalidad para ordenar por estado y id(nombre)

--- js/buscador.js
+++ js/buscador.js
@@ -39,30 +39,42 @@ $(document).ready(function() {
 
     for (var i = 0; i < data.length; i++) {
       var product = $("#product-" + data[i].ID + " .fa-clock-o");
+      var time = $("#product-" + data[i].ID + " .temporada");
 
-      console.log(
-        data[i].ID + " " + data[i].PRODUCTO + " " + data[i][month.toUpperCase()]
-      );
       product.removeClass(
         "temporada inicio-temporada fin-temporada fuera-temporada"
       );
       switch (data[i][month.toUpperCase()]) {
         case "X":
-          product.addClass("temporada");
+          product.addClass("en-temporada");
+          time.text(1);
           break;
+        //TODO remove
+        case "Y":
         case "I":
           product.addClass("inicio-temporada");
+          time.text(2);
           break;
         case "F":
           product.addClass("fin-temporada");
-          break;
-        case "Y":
-          product.addClass("inicio-temporada");
+          time.text(3);
           break;
         default:
           product.addClass("fuera-temporada");
+          time.text(4);
       }
     }
+
+    var $grid = $(".grid").isotope({
+      itemSelector: ".portfolio-item",
+      layoutMode: "fitRows",
+      getSortData: {
+        id: ".id parseInt",
+        temporada: ".temporada parseInt"
+      },
+      sortBy: "temporada"
+    });
+    $grid.isotope("updateSortData").isotope();
   }
   function loadData(province, callback) {
     console.log("Cargando datos de " + province);
