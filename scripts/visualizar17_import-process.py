a7ddb862cd81607b23a9e44c41867d0e471c656d
Rai <rai@ingmechs.com>
2017-09-27 18:55:33 +0200

    Added import export by month

---
+++ scripts/visualizar17_import-process.py
@@ -0,0 +1,48 @@
+
+import os
+import pandas as pd
+import numpy as np
+
+product = 'acelga/'
+pro = 'production/'
+imp = 'import/'
+pwd = '../data/producto/'
+aux = '../data/aux/'
+dwd = '../data/source/'
+
+
+os.chdir(dwd)
+files = os.listdir(dwd)
+
+data = pd.read_excel(files[-2], sheetname='2015', encoding ='utf-8',skiprows=[2],index_col=0,na_values='x')
+data.fillna(0,inplace=True)
+
+
+data.drop('%cuota',axis=0,inplace=True)
+data.drop('TOTAL HORTALIZAS',axis=0,inplace=True)
+data.drop('OTRAS HORTALIZAS',axis=0,inplace=True)
+data.drop('OTROS CÍTRICOS',axis=0,inplace=True)
+data.drop('OTRAS FRUTAS',axis=0,inplace=True)
+data.drop('TOTAL FRUTAS',axis=0,inplace=True)
+data.drop('TOTAL F. Y H.',axis=0,inplace=True)
+data.drop('TOTAL',axis=1,inplace=True)
+data.drop('TOTAL UE-27',axis=1,inplace=True)
+data.drop('T. HORTALIZAS',axis=0,inplace=True)
+data.drop('* Datos provisionales',axis=0,inplace=True)
+
+
+data = data.loc[pd.notnull(data.index)]
+
+
+data.index = [x.lower() for x in data.index.values.tolist()]
+
+for producto in data.index.values:
+    datos = data.loc[data.index==producto].transpose().sort_values(producto,ascending=False)
+    datos.columns = ['Importado']
+    datos[:3].to_csv(pwd+producto+'.csv')
+    datos = pd.DataFrame(datos.sum())
+    datos.columns = ['P-t']
+    datos.to_csv(pwd+producto+'_total.csv')
+
+
+datos.columns = ['P-t']
--- scripts/visualizar17_produccion-generic (7).py
+++ scripts/visualizar17_produccion-generic (7).py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[430]:
+# In[1]:
 
 import matplotlib.pyplot as plt
 import matplotlib
@@ -20,7 +20,7 @@ import ipywidgets
 from IPython.display import display
 
 
-# In[1106]:
+# In[2]:
 
 product = 'acelga'
 tipo = 'production/'
@@ -28,12 +28,12 @@ pwd = '/Volumes/Macintosh HD/_GitHub/journey-of-food/data/producto/'
 aux = '/Volumes/Macintosh HD/_GitHub/journey-of-food/data/aux/'
 
 
-# In[1107]:
+# In[3]:
 
 tipos = os.listdir(pwd)[1:]
 
 
-# In[1108]:
+# In[4]:
 
 dropdown = ipywidgets.Dropdown(
     options = tipos,
@@ -46,13 +46,13 @@ def on_change(change):
 dropdown.observe(on_change)
 
 
-# In[1841]:
+# In[269]:
 
 product = dropdown.value
 productPATH = product + '/'
 
 
-# In[1842]:
+# In[270]:
 
 products = os.listdir(pwd)
 os.chdir(pwd+productPATH+tipo)
@@ -60,43 +60,43 @@ files = glob.glob(pwd+productPATH+tipo+'*.xlsx')
 files
 
 
-# In[1845]:
+# In[277]:
 
-data = pd.read_excel(files[0],skiprows=[0,1,2,3,4,5], encoding ='utf-8')
+data = pd.read_excel(files[0],skiprows=[0,1,2,3,4,5,6], encoding ='utf-8')
 data.fillna(0,inplace=True)
 
 
-# In[1846]:
+# In[278]:
 
 data.head(2)
 
 
-# In[1847]:
+# In[279]:
 
 data[data.columns[0]].head()
 
 
-# In[1848]:
+# In[280]:
 
 data = pd.DataFrame([data[data.columns[0]], data[data.columns[-1]]]).transpose()
 
 
-# In[1849]:
+# In[281]:
 
 data.head()
 
 
-# In[1850]:
+# In[282]:
 
 data.columns = ['Provincia','P-t']
 
 
-# In[1851]:
+# In[283]:
 
 data.Provincia.unique()
 
 
-# In[1852]:
+# In[284]:
 
 data = data.loc[data.Provincia!=0]
 data = data.loc[data['Provincia'] != ' GALICIA']
@@ -115,101 +115,113 @@ data = data.loc[data['Provincia'] != ' ANDALUCIA']
 data = data.loc[data['Provincia'] != ' CANARIAS']
 data = data.loc[data['Provincia'] != ' C. VALENCIANA']
 data = data.loc[data['Provincia'] != 'ESPAÑA']
-data = data.loc[data['Provincia'] != 'Las Palmas']
-data = data.loc[data['Provincia'] != 'S.C. de Tenerife']
+#data = data.loc[data['Provincia'] != 'Las Palmas']
+#data = data.loc[data['Provincia'] != 'S.C. de Tenerife']
 
 
-# In[1853]:
+# In[285]:
 
 data['P-t'].replace('–',0,inplace=True)
 data.head()
 
 
-# In[1854]:
+# In[286]:
+
+data.Provincia.values
+
+
+# In[287]:
+
+dataCanarias = data[(data.Provincia =='Las Palmas') | (data.Provincia == 'S.C. de Tenerife')]
+data = data[(data.Provincia !='Las Palmas') & (data.Provincia != 'S.C. de Tenerife')]
+dataCanarias.head()
+
+
+# In[288]:
 
 provincias = pd.read_csv(aux+'provincias.csv', delimiter='\t',index_col=1)
 
 
-# In[1855]:
+# In[289]:
 
 data.Provincia = data.Provincia.apply(lambda x: x.upper())
 
 
-# In[1856]:
+# In[290]:
 
 data.sort_values('P-t',ascending=False).head(1)
 
 
-# In[1857]:
+# In[291]:
 
 data['INE'] = data.Provincia.map(provincias.ID)
 
 
-# In[1858]:
+# In[292]:
 
 data.sort_values('P-t',ascending=False).head(1)
 
 
-# In[1859]:
+# In[293]:
 
 provincias_name = provincias = pd.read_csv(aux+'provincias_nombre.csv',index_col=1)
 
 
-# In[1860]:
+# In[294]:
 
 data.Provincia = data.INE.map(provincias_name.ID)
 
 
-# In[1861]:
+# In[295]:
 
 datos = data.sort_values('P-t',ascending=False).head(1)
 
 
-# In[1862]:
+# In[296]:
 
 datos.head()
 
 
-# In[1863]:
+# In[297]:
 
 datos.to_csv(pwd+productPATH+tipo+'principal_provincia_peninsula.csv',encoding='utf-8')
 
 
-# In[1864]:
+# In[298]:
 
 #data = data.loc[data.index<73]
 
 
-# In[1865]:
+# In[299]:
 
 data.sort_values('P-t',ascending=False,inplace=True)
 
 
-# In[1866]:
+# In[300]:
 
 head = data[:3]
 tail = data[3:]
 
 
-# In[1867]:
+# In[301]:
 
 tail = pd.DataFrame(tail.sum()).transpose()
-tail.Provincia = 'Otros'
+tail.Provincia = 'Resto de España'
 tail.INE = 0
 tail.head()
 
 
-# In[1868]:
+# In[302]:
 
 grafico = pd.concat([head,tail])
 
 
-# In[1869]:
+# In[303]:
 
 grafico.set_index('Provincia',inplace=True)
 
 
-# In[1870]:
+# In[304]:
 
 grafico['Percent'] = grafico['P-t']/sum(grafico['P-t'])*100.00
 grafico.Percent = grafico.Percent.astype('float')
@@ -217,64 +229,93 @@ grafico.Percent = grafico.Percent.round(2)
 grafico.head()
 
 
-# In[1871]:
+# In[305]:
 
 grafico.to_csv(pwd+productPATH+tipo+'grafico.csv', encoding='utf-8')
 
 
-# In[1872]:
+# In[306]:
 
 grafico['Percent'].plot('barh')
 plt.xlabel('Percent')
 plt.savefig(product+'.png')
 
 
-# In[1873]:
+# In[307]:
 
 data.to_csv(pwd+productPATH+tipo+'production_ready_INE.csv', encoding='utf-8')
 
 
-# In[1874]:
+# In[308]:
 
 data.head()
 
 
-# In[1875]:
+# In[309]:
 
 total = pd.DataFrame(pd.DataFrame(data.sum()).transpose())
 total.Provincia = 'España'
 total.INE = 0
+total.drop('INE',axis = 1, inplace=True)
 total
 
 
-# In[1876]:
+# In[310]:
+
+try:
+    dataCanarias = pd.DataFrame(dataCanarias.sum()).transpose()
+except:
+    print('No datos canarias')
+
+
+# In[311]:
+
+dataCanarias.Provincia = 'Canarias'
+
+
+# In[312]:
+
+total = total.append(dataCanarias)
+
+
+# In[313]:
+
+total.set_index('Provincia',inplace=True)
+
+
+# In[314]:
+
+total.head()
+
+
+# In[315]:
 
 total.to_csv(pwd+productPATH+tipo+'total.csv', encoding='utf-8')
 
 
-# In[1877]:
+# In[316]:
 
 #dataMelt = pd.melt(data, id_vars='Provincia', value_vars=['S-ha_15/12-15/4','P-t_15/12-15/4','S-ha_15/4-15/6','P-t_15/4-15/6','S-ha_15/6-15/9','P-t_15/6-15/9','S-ha_15/9-15/1','P-t_15/9-15/1'])
 
 
-# In[1701]:
+# In[171]:
 
 #dataMelt['Unit'] = dataMelt['variable']
 #dataMelt['Unit'] = dataMelt['Unit'].apply(lambda x: x.split('_')[0])
 #dataMelt['variable'] = dataMelt['variable'].apply(lambda x: x.split('_')[1])
 
 
-# In[ ]:
+# In[172]:
 
 #dataMelt.columns =['Provincia', 'Variable', 'Cantidad', 'Unit']
 
 
-# In[ ]:
+# In[173]:
 
 #dataMelt['nonumeric'] = dataMelt.Cantidad.apply(lambda x: isinstance(x, int))
 
 
-# In[ ]:
+# In[174]:
 
 #dataMelt = dataMelt.loc[dataMelt.nonumeric == True]
 
