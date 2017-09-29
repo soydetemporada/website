13af78dab4cff99516a6f5cd2ef4ea4281e517a9
Sergio <sergio@tinkerista.com>
2017-09-29 14:30:21 +0200

    grafico

--- js/graficos.js
+++ js/graficos.js
@@ -1,60 +1,130 @@
 
+var meses={
+    'Enero':0,'Febrero':1,'Marzo':2,'Abril':3,'Mayo':4,'Junio':5,'Julio':6,'Agosto':7,'Septiembre':8,'Octubre':9,'Noviembre':10,'Diciembre':11
+}
+
 function graficaImportExport(container,path){
-  var svg = d3.select(container),
-    margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      var svg = d3.select(container),
+        margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = +svg.attr("width") - margin.left - margin.right,
+        height = +svg.attr("height") - margin.top - margin.bottom,
+        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        //var parseTime = d3.timeParse("%b");
+
+    var parseRow=function(d) {
+          d.Mes = meses[d.Mes]
+          d.Importado = d.Importado
+          d.Exportado = d.Exportado;
+          return d;
+    }
+
+    var xx = d3.scaleLinear().range([10, width]);
+    var yy = d3.scaleLinear().range([ height,0]);
+
+    var xaxis=  d3.axisBottom(xx).ticks(12).tickFormat(function(d){
+        return Object.keys(meses)[d]
+    })
+    //.ticks(d3.timeMonths).tickSize(16, 0)
+    //.tickFormat(d3.timeFormat("%B"));
+//    xaxis.tickValues(Object.keys(meses));//.tickFormat(d3.timeFormat("%m"));
+    var lineImport = d3.line()
+      .x(function(d) {  return xx(d.Mes); })
+      .y(function(d) { return yy(d.Importado); });
+
+      var lineExport = d3.line()
+        .x(function(d) { return xx(d.Mes); })
+        .y(function(d) {  return yy(d.Exportado); });
+
+    d3.csv(path, parseRow,function(error, data) {
+            mdata=data
+          if (error) throw error;
+          maxImport=d3.max(data,function(d){return parseInt(d.Importado)})
+          maxExport=d3.max(data,function(d){return parseInt(d.Exportado)})
+          xx.domain([0,11]);
+          yy.domain([0,1.1*d3.max([maxImport, maxExport])]);
+
+          // .tickFormat(d3.time.format("%B"))
+          g.append("g")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xaxis )
+            .select(".domain")
+              .remove();
+
+          g.append("g")
+             .call(d3.axisLeft(yy))
+            .append("text") //texto
+              .attr("fill", "#000")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", "0.71em")
+              .attr("text-anchor", "end")
+              .text("Toneladas");
 
 
-    var parseTime = d3.timeParse("%b");
+          g.append("path")
+              .data([data])
+              .classed("line import",true).transition()
+              .attr("d", lineImport);
 
-    var x = d3.scaleTime()
-        .rangeRound([0, width]);
+          g.append("path")
+              .data([data])
+              .classed("line export",true).transition()
+              .attr("d", lineExport);
 
-var y = d3.scaleLinear()
-    .rangeRound([height, 0]);
+              d3.selectAll("body").append("div")
+              .classed('d3-tooltip',true);
+/*** import label **/
+        g.append("text").text("Importación").attr("transform", function(){
+            return "translate(" + xx(data[1].Mes) + "," + (yy(data[11].Importado) -20) + ")";
+        })
 
-var line = d3.line()
-    .x(function(d) { return x(d.Mes); })
-    .y(function(d) { return y(d.Importado); });
+        var g1 = g.selectAll("g.dotImport") //g1 is the update section
+              .data(data)
 
-d3.csv(path, function(d) {
-  d.Mes = parseTime(d.Mes);
-  d.Importado = +d.Importado
-  d.Exportado = +d.Exportado;
-  return d;
-}, function(error, data) {
-  if (error) throw error;
-console.log(data)
-  x.domain(d3.extent(data, function(d) { return d.Mes; }));
-  y.domain(d3.extent(data, function(d) { return d.Importado; }));
+        var element = g1.enter().append("g").attr("transform", function(d) {
+            return "translate(" + xx(d.Mes) + "," + yy(d.Importado) + ")";
+        })
+        element.classed("dotImport ", true);
+        element.append("svg:circle")
+        .attr('r',4)
 
-  g.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
-    .select(".domain")
-      .remove();
 
-  g.append("g")
-      .call(d3.axisLeft(y))
-    .append("text")
-      .attr("fill", "#000")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text("Price ($)");
+/*** export label **/
+        g.append("text").text("Exportación").attr("transform", function(){
+            return "translate(" + xx(data[1].Mes) + "," + (yy(data[11].Exportado) -20) + ")";
+        })
+        var g1 = g.selectAll("g.dotExport") //g1 is the update section
+              .data(data)
 
-  g.append("path")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-      .attr("stroke-width", 1.5)
-      .attr("d", line);
-});
+        var element = g1.enter().append("g").attr("transform", function(d) {
+            return "translate(" + xx(d.Mes) + "," + yy(d.Exportado) + ")";
+        })
+        element.classed("dotExport", true);
+        element.append("svg:circle")
+        .attr('r',0)
+        .transition(300).delay(500)
+        .attr('r',5)
+
+        element.on("mouseover", function(d,i) {
+/*
+            $(".d3-tooltip").css('top',d3.mouse(svg)[1]+"px")
+            $(".d3-tooltip").css('left',d3.mouse(svg)[0]+"px")
+
+            $(".d3-tooltip").show();
+            $(".d3-tooltip").html(d.Exportado);*/
+
+
+
+        })
+
+        element.on("mouseout", function(d,i) {
+            $(".d3-tooltip").hide();
+        //    d3.select(this).select("circle")
+        })
+
+
+
+    });
 }
 
 
