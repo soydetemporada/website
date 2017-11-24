887ec83b921ff8bca21089dfc4d11741fa333f7f
Rai <rai@ingmechs.com>
2017-11-24 12:21:06 +0100

    Added new calendar script

--- _scripts/visualizar17-calendario.py
+++ _scripts/visualizar17-calendario.py
@@ -28,9 +28,11 @@ XLIM = 0.5
 # Get paths
 
 currentPWD = os.getcwd()
-dwd = currentPWD[:-7]+'data/seasons/'
-aux = currentPWD[:-7]+'data/aux/'
-pwd = currentPWD[:-7]+'data/calendar/'
+currentPWD = '/Volumes/MacintoshHD/_GitHub/journey-of-food/scripts'
+
+dwd = currentPWD[:-7]+'/data/seasons/'
+aux = currentPWD[:-7]+'/data/aux/'
+pwd = currentPWD[:-7]+'/data/calendar/'
 
 # List calendar files available
 
@@ -73,17 +75,51 @@ data.fillna(0,inplace=True)
 #Name columns
 
 data['SCORE']=data.ENE+data.FEB+data.MAR+data.ABR+data.MAY+data.JUN+data.JUL+data.AGO+data.SEP+data.OCT+data.NOV+data.DIC
+data['TIPO DE PRODUCTO'] = data['TIPO DE PRODUCTO'].apply(lambda x: x.lower())
+data['TIPO DE PRODUCTO'] = data['TIPO DE PRODUCTO'].apply(lambda x: x.replace('ó','o'))
+data['TIPO DE PRODUCTO'] = data['TIPO DE PRODUCTO'].apply(lambda x: x.replace('á','a'))
+data['TIPO DE PRODUCTO'] = data['TIPO DE PRODUCTO'].apply(lambda x: x.replace('é','e'))
+data['TIPO DE PRODUCTO'] = data['TIPO DE PRODUCTO'].apply(lambda x: x.replace('í','i'))
+data['TIPO DE PRODUCTO'] = data['TIPO DE PRODUCTO'].apply(lambda x: x.replace('ú','u'))
+data['TIPO DE PRODUCTO'] = data['TIPO DE PRODUCTO'].apply(lambda x: x.replace('ñ','n'))
+data['TIPO DE PRODUCTO'] = data['TIPO DE PRODUCTO'].apply(lambda x: x.replace('habas','haba'))
+data['TIPO DE PRODUCTO'] = data['TIPO DE PRODUCTO'].apply(lambda x: x.replace('maiz dulce','maiz'))
+data['TIPO DE PRODUCTO'] = data['TIPO DE PRODUCTO'].apply(lambda x: x.replace('guisantes','guisante'))
+data['TIPO DE PRODUCTO'] = data['TIPO DE PRODUCTO'].apply(lambda x: x.replace('higos','higo'))
+data['TIPO DE PRODUCTO'] = data['TIPO DE PRODUCTO'].apply(lambda x: x.replace('judias','judia'))
+data['TIPO DE PRODUCTO'] = data['TIPO DE PRODUCTO'].apply(lambda x: x.replace(' ','-'))
+data['TIPO DE PRODUCTO'] = data['TIPO DE PRODUCTO'].apply(lambda x: x.replace('col-comun','col'))
+data['TIPO DE PRODUCTO'] = data['TIPO DE PRODUCTO'].apply(lambda x: x.replace('cabalaza','calabaza'))
 
-#Only use answers with values
 
 data = data.loc[data.SCORE>0]
 
+data = data.reset_index(drop=True)
+
+# Ensure products
+#data['TIPO DE PRODUCTO'].unique()
+
+_products = os.listdir(currentPWD[:-7]+'/_products')
+_productList = []
+
+for fichero in _products:
+    if fichero.endswith('.markdown'):
+        _productList.append(fichero.split('.')[0])
+#_productList
+data['drop'] = data['TIPO DE PRODUCTO'].apply(lambda x: x in _productList)
+data = data[data['drop']==True]
+#Only use answers with values
+
+
+
 # Pivot table
 
-pivot = data.groupby(data.index).mean()
+pivot = data.groupby('TIPO DE PRODUCTO').mean()
 
 pivot = pivot.round(2)
 
+pivot['COUNT'] = data.groupby('TIPO DE PRODUCTO')['SCORE'].count()
+pivot
 # Map according to limits
 
 def mapear(x):
@@ -109,12 +145,15 @@ pivot.DIC = pivot.DIC.apply(lambda x: mapear(x))
 
 
 # Fix names
-names = pd.read_csv(aux+'ID_name.csv', encoding ='utf-8', delimiter = ',', index_col=0)
-serie = pd.Series(pivot.index.values)
-serie = serie.map(names.NAME)
-pivot.index = serie
+#names = pd.read_csv(aux+'ID_name.csv', encoding ='utf-8', delimiter = ',', index_col=0)
+#serie = pd.Series(pivot.index.values)
+#serie = serie.map(names.NAME)
+#pivot.index = serie
+
 pivot.index.name = 'PRODUCTO'
-pivto = pivot.sort_index(inplace=True)
+pivot.sort_index(inplace=True)
+
+pivot.drop('drop',axis=1,inplace=True)
 
 # Save to file
 pivot.to_csv(dwd+'calendario_py.csv',encoding='utf-8')
